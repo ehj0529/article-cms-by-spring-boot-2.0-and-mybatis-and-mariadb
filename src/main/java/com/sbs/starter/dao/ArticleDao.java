89374e7330ca192f8b168e2b3751fe897538c30e
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2020-04-20 22:39:43 +0900

    ArticleDaoImpl를 제거하고 마이바티스를 통해 DB연동하도록 처리

--- src/main/java/com/sbs/starter/dao/ArticleDao.java
+++ src/main/java/com/sbs/starter/dao/ArticleDao.java
@@ -1,9 +1,10 @@
 package com.sbs.starter.dao;
 
 import java.util.List;
-
+import org.apache.ibatis.annotations.Mapper;
 import com.sbs.starter.dto.Article;
 
+@Mapper //ArticleDao의 구현체를 마이바티스가 대신 구현해준다.
 public interface ArticleDao {
 	
 	public List<Article> getList();
