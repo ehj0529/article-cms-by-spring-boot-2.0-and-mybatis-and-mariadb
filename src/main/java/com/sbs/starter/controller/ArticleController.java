03b8ded816347cd9776c8125e9b7b3a1710bf258
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2020-04-21 22:05:52 +0900

    게시물 추가 후 상세페이지로 이동되도록 처리(상세패이지는 아직 임.)

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -47,8 +47,18 @@ public class ArticleController {
 	public String doAdd(@RequestParam Map<String, Object> param) { //String title, String body
 
 		long newId = articleService.add(param);
-		return newId +"번 게시물이 추가되었습니다.";
+		String msg = newId +"번 게시물이 추가되었습니다.";
 		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("alert('"+msg+"');");
+		sb.append("location.replace('.detail?id="+newId+"');");
+
+		sb.insert(0,"<script>");
+		sb.append("</script>");
+		
+		
+		return sb.toString();
 	}
 
 
